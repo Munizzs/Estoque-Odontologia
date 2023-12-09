9630f9f88c2703ca3cb5dc8b7dfc880f6ded15ba
Matheus Muniz <matheusmuniz0810@gmail.com>
2023-12-09 19:26:38 -0300

    Criado um metodo para mostrar os produtos e mais 3 opções a o adicionar/remover produto. Update em remover produto e mostrar produto opção (2 e 3)

--- odontoEstoque.java
+++ odontoEstoque.java
@@ -5,7 +5,7 @@ static Scanner ler = new Scanner(System.in);
     
 public static void ShowProdutos() {        
         System.out.println("""
-            Digite o numero do produto que voce deseja adicionar(Digite 0 para fechar):
+            Digite o numero do produto que voce deseja selecionar(Digite 0 para fechar):
                            
                 ------------Tabela dos Produtos------------
                            
@@ -22,20 +22,11 @@ public static void ShowProdutos() {
         System.out.println("\n Escolha:");
     }
 
-   public static int[] inicializarEstoque() {
-        int[] estoque = new int[10];
-        for (int i = 0; i < estoque.length; i++) {
-            estoque[i] = 0;
-        }
-        return estoque;
-    }
-    
-    
-
     public static void main(String[] args) {
         
-        int qtd = 0, o, rp, rm1 = 0, rm2,  pe, pq, valorT = 0;
-        int[] estoque = inicializarEstoque();          
+        int o,p,v,e = 0,r;
+        
+        int valor[] = new int[10];
 
         String ProS[] = new String[10];//{"Bandeja","Espelho cli­nico","Pinça","Escovador","Sonda exploradora","Esculpidor Hollemback","Seringa Carpule","Placa de vidro","Cubas","Pote Dappen",};
         ProS[0] = "Bandeja";
@@ -48,90 +39,108 @@ public static void ShowProdutos() {
         ProS[7] = "Placa de vidro";
         ProS[8] = "Cubas";
         ProS[9] = "Pote Dappen";
-
-        System.out.println("--------------------Estoque--------------------");
-        do {
-            System.out.println("\n\nEscolha:\n1- Para repor estoque\n2- Retirar estoque\n3- Verificar estoque\n4- Fechar");
+                
+      do{  
+      System.out.println("\n\nEscolha:\n1- Adicionar/Repor estoque\n2- Remover estoque\n3- Verificar estoque\n4- Fechar");
             o = ler.nextInt();
             switch (o) {
-                case 1 -> {
-                    do {
+                case 1 -> {  
+       do{
+           ShowProdutos();       
+            p=ler.nextInt();
+        if(p==0){break;}  
+        if(p>0 && p<11){  
+            do{
+            System.out.printf("\nVoce selecionou %d. %s\n\nQuantos voce quer adicionar?(Digite 0 para voltar) ",p,ProS[p-1]);
+            v=ler.nextInt();
+                if(v<0){System.err.println("Quantidade invalida."); continue;}            
+            
+            valor[p-1] = valor[p-1]+v;              
+            
+            if(v==0){System.out.printf("\nAdicionado com sucesso!\n\n");break;}
+            
+            System.out.printf("Voce adicionou %d %s",v,ProS[p-1]);
+            do{
+                System.out.println("\n\nEscolha:\n |1-Continuar | 2-Escolher outro produto| 3-Voltar para o menu|");
+                e=ler.nextInt();
+                    
+                        if(e==1){                           
+                            break;
+                        }else if(e==2){
+                            break;
+                        }else if(e==3){
+                            break;
+                        }else{
+                            continue;
+                        }
+                    }while(true);
+            if(e==2){break;}
+            if(e==3){break;}
+            }while(true);
+            if(e==3){break;}
+            
+        }else{       
+            System.err.println("Opção Invalida.\n");
+            continue;        
+        }  
+            
+       }while(true);
+                }
+                case 2 ->{
+                    do{ 
                         ShowProdutos();
-                        int p = ler.nextInt();                       
-                       
-
-                        if (p >= 1 && p <= 10) {
-                            int quantidadeAtual = estoque[p - 1];
-                            System.out.printf("\nVoce escolheu %d - %s", p, ProS[p-1]);
-
-                            System.out.println("\n\nQuantos voce quer adicionar/repor?(Digite 0 para fechar)");
-                            rp = ler.nextInt();
-                            quantidadeAtual += rp;
-                            estoque[p - 1] = quantidadeAtual;
-                            System.out.println("---\nVoce adicionou " + rp + " " + ProS[p-1] + "\n\nQuantidade total de " + ProS[p-1] + ": " + quantidadeAtual+"\n---");
-                            //Resolver a quantidade armazenada em cada produto
-                        } else if (p == 0) {
-                            System.out.println("\n\nAdicionado com sucesso!\nArmazenado " + qtd);
+            p=ler.nextInt();
+        if(p==0){break;}  
+        if(p>0 && p<11){  
+            do{
+            System.out.printf("\nVoce selecionou %d. %s\n\nQuantos voce quer remover?(Digite 0 para voltar) ",p,ProS[p-1]);
+            r=ler.nextInt();
+                if(r<0){System.err.println("Quantidade invalida."); continue;}
+            
+            valor[p-1] = valor[p-1]-r;                               
+            
+            if(r==0){System.out.printf("\nRemovido com sucesso!\n\n");break;}
+                if(valor[p-1]<r){System.err.println("Quantidade insuficiente."); continue;} 
+            
+            System.out.printf("Voce removeu %d %s",r,ProS[p-1]);
+            do{
+                System.out.println("\n\nEscolha:\n |1-Continuar | 2-Escolher outro produto| 3-Voltar para o menu|");
+                e=ler.nextInt();
+                    
+                        if(e==1){                           
                             break;
-                        } else {
-                            System.out.println("Opção invalida!");
+                        }else if(e==2){
+                            break;
+                        }else if(e==3){
+                            break;
+                        }else{
+                            continue;
                         }
-                    } while (true);
+                    }while(true);
+            if(e==2){break;}
+            if(e==3){break;}
+            }while(true);
+            if(e==3){break;}
+            
+        }else{       
+            System.err.println("Opção Invalida.\n");
+            continue;        
+        }              
+       }while(true);}
+                case 3 ->{
+                   int n = 0;
+                        System.out.printf("\n----------------------------------");
+                    for(int produtos : valor) {
+                        System.out.printf("\n|%2d - %-22s: %-3d|",n+1,ProS[n],produtos);
+                        n++; 
+                    }
+                    System.out.printf("\n----------------------------------");
                 }
-                case 2 -> {
-                    do {
-                        ShowProdutos();
-                        int p = ler.nextInt(); 
-                        if(p >= 1 && p<=10){
-                            
-                        int quantidadeAtual = estoque[p - 1];
-                            System.out.printf("\nVoce escolheu %d - %s", p, ProS[p-1]);    
-                        
-                        System.out.println("\n\nQuantos voce quer retirar?(Digite 0 para fechar)");
-                        rm1 = ler.nextInt();
-                        
-                        quantidadeAtual -= rm1;
-                        estoque[p - 1] = quantidadeAtual;
-                        
-                        if (quantidadeAtual < 0) {
-                            System.out.println("Quantidade insuficiente");
-                            break;
-                        }
-                        System.out.println("\nVoce removeu " + rm1 + "\nQuantidade " + estoque[p - 1]);
-                        
-                        }else if (rm1 == 0) {
-                            System.out.println("\n\nRemovido com sucesso!");
-                            break;
-                        }else {
-                            System.out.println("Opção invalida!");
-                        }
-                       /* System.out.println("\n\nQuantos voce quer retirar?(Digite 0 para fechar)");
-                        rm1 = ler.nextInt();
-                        qtd = qtd - rm1;
-                        if (qtd < 0) {
-                            System.out.println("Quantidade insuficiente");
-                            break;
-                        }
-                        System.out.println("\nVoce removeu " + rm1 + "\nQuantidade " + qtd);
-                        if (rm1 == 0) {
-                            System.out.println("\n\nRemovido com sucesso!\nArmazenado " + qtd);
-                            break;
-                        }*/
-                    } while (true);
-                }
-                case 3 -> {
-
-                }
-                case 4 -> {
-                    System.out.println("Voce saiu do sistema!");
-                    return;
-
-                }
-                default -> {
-                    System.out.println("Opção inválida, digite novamente!");
-
-                }
-            }
-        } while (true);
+                case 4 ->{System.out.close();}
+                default->{}
     }
-}
+            if(o==4){break;}
+    }while(true);
+      
+      }   
+    }
